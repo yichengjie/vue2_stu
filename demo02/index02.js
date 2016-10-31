f4584f3dac3f9c254c3b17332e6d2d64a13ad400
yichengjie <626659321@qq.com>
2016-10-31 11:35:56 +0800

    s

--- demo02/index02.js
+++ demo02/index02.js
@@ -17,10 +17,32 @@ Vue.component('my-input',{
     }
 }) ;
 
+
+Vue.component('my-upper',{
+    template:`
+        <div class ="form-group">
+            <label v-bind:for="randomId" class ="control-label">{{label}}</label>
+            <input type="text" v-bind:id="randomId" v-bind:value="value" v-on:input="onInput" class ="form-control"/>
+        </div>`,
+    props:['value','label'],
+    data:function(){
+        return {
+            randomId:'input-'+Math.random() 
+        };
+    },
+    methods:{
+        onInput:function(event){
+            var str = event.target.value.toUpperCase() ;
+            this.$emit('input',str) ;
+        }
+    }
+}) ;
+
 new Vue({
     el:'#app',
     data:{
         message:'hello',
-        message2:'hello2'
+        message2:'hello2',
+        subcode:''
     }
 }) ;
