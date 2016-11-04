e4b0b6d629a90be05be87bf1418123988301b000
yichengjie <626659321@qq.com>
2016-11-04 10:26:54 +0800

    s

--- demo04/src/store/getters.js
+++ demo04/src/store/getters.js
@@ -3,7 +3,8 @@ const getters = {
    listCount:state => state.records7List.length ,
    records7List:state => state.records7List ,
    pageBar:state => state.pageBar ,
-   queryDBFlag:state => state.queryDBFlag
+   queryDBFlag:state => state.queryDBFlag,
+   dealPageOrderFlag:state => state.dealPageOrderFlag
 }
 
 export default getters ;
