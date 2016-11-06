a6cdfe2b13863824bc92bb95775296f92f13ca8c
yichengjie <626659321@qq.com>
2016-11-06 21:29:11 +0800

    s

--- demo05/src/App.vue
+++ demo05/src/App.vue
@@ -2,23 +2,13 @@
     <div>
         <Navbar active-name = "附加服务"/>
         <QuerySection/>
-        <div class="container-fluid main_content" id="main_content" style="margin-top:10px;" >
-            
-            <div class="allInfo_descr pull-left" v-show ="pageBar.isQueryDB">
-                <input type="checkbox" id ="queryDBFlagCheckbox" v-bind:checked="queryDBFlag"  
-                    v-on:click ="clickQueryDBFlag"/>
-                <label for="queryDBFlagCheckbox">共查询出<span class="red">{{pageBar.recordCount}}</span>条记录，针对全部记录排序?</label>
-            </div>
-
+        <div class="container-fluid main_content" style="margin-top:10px;" >
+            <ResultDescribe/>
             <Operbar />
-
             <TableList />
-
             <Pagebar/>
         </div>
     </div>
-    
-
 </template>
 <script>
     import Navbar from './components/Navbar.vue' ;
@@ -26,38 +16,21 @@
     import Operbar from './components/Operbar.vue' ;
     import TableList from './components/TableList.vue' ;
     import Pagebar from './components/Pagebar.vue' ;
+    import ResultDescribe from './components/ResultDescribe.vue' ;
     import { mapGetters, mapActions } from 'vuex' ;
     export default{
         name:'app',
-        data (){
-            return {
-                message:'component from App.vue'
-            };
-        },
         components:{
             Navbar,
             QuerySection,
             Operbar,
             TableList,
-            Pagebar
-        },
-        methods:{
-            clickQueryDBFlag(event){
-                let queryDBFlag = event.target.checked ;
-                //console.info('queryDBFlag : ' + queryDBFlag) ;
-                this.updateSimpleState({queryDBFlag}) ;
-            },
-            ...mapActions([
-                'updateSimpleState'
-            ])
-        },
-        computed: mapGetters([
-          'pageBar',
-          'queryDBFlag'
-        ]),    
+            Pagebar,
+            ResultDescribe
+        }
     }
 </script>
-<style>
+<style scoped>
    body{
      min-width: 1300px;
    }
