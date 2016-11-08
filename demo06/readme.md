aa8b3a3a86eed991eea607eaa5ee6e9be86873fe
yichengjie <626659321@qq.com>
2016-11-08 17:50:45 +0800

    s

---
+++ demo06/readme.md
@@ -0,0 +1,2 @@
+# 在demo04的基础上改善现有的代码细节，更加的与oc真实项目一致
+> 注意webpack.config.js最vue别用的配置，否则直接import Vue from  'vue'会有问题
---
+++ demo06/webpack.config.prod.js
@@ -0,0 +1,56 @@
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var path = require('path') ;
+var ASSETS_PATH = path.resolve('./src/assets');
+var LIB_PATH = path.resolve('./src/lib');
+var webpack = require('webpack') ;
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: __dirname+"/dist/",
+    filename: '[name].js'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {test: /\.vue$/,loader: 'vue'},
+      {test: /\.js$/,loader: 'babel',exclude: /node_modules|lib/},
+      {test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader") },
+      {test: /\.(jpg|png|gif)$/, loader: "url?limit=8192&name=./images/[name].[hash:8].[ext]"},
+      {test: /\.(eot|woff|ttf|svg)$/, loader: "file-loader?name=./file/[name].[hash:8].[ext]" }
+    ]
+  },
+  resolve: {
+      alias: {
+        'vue': 'vue/dist/vue.js',
+        'assets': ASSETS_PATH,
+        'tui_core_lib':LIB_PATH+"/tui-core/index.js",
+        'tui_dialog_lib':LIB_PATH+"/tui-dialog/index.js",
+        'tui_drag_lib':LIB_PATH+"/tui-drag/index.js",
+        'jq_datepicker_lib': LIB_PATH+"/jq-datepicker/index.js",
+	      'jq_timepicker_lib':LIB_PATH+"/jq-timepicker/index.js",
+        'modal_lib':LIB_PATH+"/modal.js",
+        'iconfont_lib':LIB_PATH+"/iconfont/iconfont.js",
+        'HttpClientUtil_lib':LIB_PATH+"/HttpClientUtil.js",
+        'util_lib':LIB_PATH+"/util.js",
+        'lodash_lib':LIB_PATH+"/lodash/lodash.js",
+        'moment_lib':LIB_PATH+"/moment/index.js",
+        'ajaxfileupload_lib':LIB_PATH+"/ajaxfileupload.js",
+        'is_loading_lib':LIB_PATH+"/is-loading/index.js",
+        'bt_growl_lib':LIB_PATH+"/bt-grow/jquery.bootstrap-growl.js"
+      }
+  },
+  plugins: [
+    new ExtractTextPlugin("[name].css"),
+     new webpack.optimize.UglifyJsPlugin({
+        compress: {
+            warnings: false
+        }
+      })
+  ],
+  vue: {
+    loaders: {
+        css: ExtractTextPlugin.extract("css")
+    }
+  }
+}
