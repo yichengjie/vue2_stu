b4d49e7d1959e0d247587bac36b60fc47e39951d
yichengjie <626659321@qq.com>
2016-11-10 17:45:40 +0800

    s

--- element01/src/App.vue
+++ element01/src/App.vue
@@ -1,11 +1,17 @@
 <template>
     <div class="container">
-        <h1>hello element ui</h1>
+       <Layout002/>
     </div>
 </template>
 <script>
-
+    //import Layout001 from './components/Layout001.vue' ;
+    import Layout002 from './components/Layout002.vue' ;
+    export default {
+        components:{
+            Layout002
+        }
+    }
 </script>
 <style>
-
+    
 </style>
