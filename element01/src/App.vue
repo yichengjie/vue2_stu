316d77973c087355ba2dca88313a5e9bab706707
yichengjie <626659321@qq.com>
2016-11-11 13:43:17 +0800

    s

--- element01/src/App.vue
+++ element01/src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-       <Form003/>
+       <Form004/>
     </div>
 </template>
 <script>
@@ -8,16 +8,17 @@
     //import Layout002 from './components/Layout002.vue' ;
     //import Form001 from './components/Form001.vue';
     //import Form002 from './components/Form002.vue';
-    import Form003 from './components/Form003.vue';
+    //import Form003 from './components/Form003.vue';
+    import Form004 from './components/Form004.vue';
     export default {
         components:{
-            Form003
+            Form004
         }
     }
 </script>
 <style>
     .container{
-        width: 500px;
+        width: 600px;
         margin: 50px auto ;
     }
 </style>
