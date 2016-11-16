7175e2b0170e18046b403bc351e91cbbbc99ad71
yichengjie <626659321@qq.com>
2016-11-16 19:59:33 +0800

    弯沉

--- element05/src/views/Form002.vue
+++ element05/src/views/Form002.vue
@@ -34,91 +34,21 @@
   import ElInput from '../components/input.vue' ;
   import ELSelect from '../components/select.vue' ;
   import ELCheckbox from '../components/checkbox.vue' ;
+  import {validateAge,validatePub,selectOption} from './Form002.js' ;
 
   //基本表单验证
   export default {
     data() {
       var age = (rule, value, callback) => {
-         var age1 = this.ruleForm.age1 ;
-         var age2 = this.ruleForm.age2 ;
-         if (age1 === '' && age2==='') {
-            callback(new Error('age1与age2至少填写一个!'));
-            return ;
-         }
-         if(isNaN(age1)){
-            callback(new Error('age1必须输入数字'));
-            return ;
-         }
-         if(isNaN(age2)){
-            callback(new Error('age2必须输入数字'));
-            return ;
-         }
-         var age11 = parseInt(age1,10) ;
-         var age22 = parseInt(age2,10) ;
-         if (age11 > age22) {
-            callback(new Error('后者要大于前者!'));
-         } else {
-            callback();
-         }
+         validateAge(callback,this) ;
       };
-
       var pub = (rule, value, callback) =>{
-         //console.info('pub **************') ;
-         var pubType = this.ruleForm.pubType ;
-         var pubValue = this.ruleForm.pubValue ;
-
-         if(pubType==='' && pubValue === ''){
-             callback();
-             return ;
-         }
-         if (pubType !== '' && pubValue==='') {
-            callback(new Error('发布对象代码必填'));
-            return ;
-         }
-         if (pubValue !== '' && pubType==='') {
-            callback(new Error('发布对象类型必填'));
-            return ;
-         }
-
-         if(pubType==='1'){
-            if(pubValue!=='1'){
-               callback(new Error('类型1时value必须为1'));
-               return ;
-            }
-         }
-
-         if(pubType==='2'){
-            if(pubValue!=='2'){
-               callback(new Error('类型2时value必须为2'));
-               return ;
-            }
-         }
-
-         if(pubType==='3'){
-            if(pubValue!=='3'){
-               callback(new Error('类型2时value必须为3'));
-               return ;
-            }
-         }
-         callback();
-         return ;
+         validatePub(callback,this) ;
       }
-
       return {
         readonly:true,
         selectOption:{
-          pubOptions:[
-            {name:"选择",value:''},
-            {name:"类型1",value:"1"},
-            {name:"类型2",value:"2"},
-            {name:"类型3",value:"3"}
-          ],
-          dayOfWeakOption:[
-            {name:"星期1",value:'1'},
-            {name:"星期2",value:"2"},
-            {name:"星期3",value:"3"},
-            {name:"星期4",value:"4"}
-          ]
+          ...selectOption
         },
         ruleForm: {
           name: '',
@@ -126,7 +56,7 @@
           age2:'',
           pubType:'2',
           pubValue:'',
-          dayOfWeak:['1','3']
+          dayOfWeak:['1']
         },
         rules: {
           name: [
