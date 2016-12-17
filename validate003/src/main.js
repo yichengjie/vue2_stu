e805207ea1d45d1834773ac77fb731e7946d48e9
yichengjie <626659321@qq.com>
2016-12-17 15:30:39 +0800

    支持动态校验

---
+++ validate003/src/main.js
@@ -0,0 +1,54 @@
+require('./style/index.css') ;
+import Validator  from './validator.js' ;
+$(function(){
+    console.info('hello world') ;
+    var myForm = document.getElementById('myForm') ;
+    var submitBtn = document.getElementById('submitBtn') ; 
+    
+    submitBtn.onclick = function(){
+        var addr = $('input[name=addr]').val() ;
+        data.addr = addr ;
+        var errorMsg = validateFunc(myForm) ;
+        if( errorMsg ){
+            alert(errorMsg) ;
+            return false;//阻止表单提交
+        }else {
+            alert('校验通过，可以提交表单') ;
+            return false;
+        }
+    }
+}) ;
+
+var data = {
+    name:'123',
+    addr:'henan'
+} ;
+
+
+var validateFunc = function(myForm){
+    var validator = new Validator() ;
+    var vfn = function(value){
+        if(value  === '' ){
+            return '不能为空';
+        }
+        if(value === '123'){
+            return '密码不能为123'  ;
+        }
+        if(data.addr === '123'){
+            return '地址不能是123' ;
+        }
+    }
+
+
+    validator.add(myForm.username,[
+        {strategy:'isNonEmpty',errorMsg:'用户名不能为空'},
+        {strategy:'minLength:6',errorMsg:'用户名长度不能小于6位'}
+    ]) ;
+
+    validator.add(myForm.password,[
+        {validator:vfn}
+    ]) ;
+    var errorMsg = validator.start() ;
+    return errorMsg ;
+}
+
---
+++ validate003/src/validator.js
@@ -0,0 +1,55 @@
+export default class Validator {
+    constructor(){
+        this.cache = [] ;
+    }
+    add (dom,rules) {
+        var self = this ;
+        for(let rule of rules){
+            (function(rule){
+                if(rule.strategy){
+                    var strategyAry = rule.strategy.split( ':' ) ;
+                    var errorMsg =  rule.errorMsg ;
+                    self.cache.push(function(){
+                        var strategy = strategyAry.shift() ;//用户挑选的stragey
+                        strategyAry.unshift(dom.value);
+                        strategyAry.push(errorMsg) ;
+                        return strategies[strategy].apply(dom,strategyAry) ;
+                    }) ;
+                }else{
+                    var fn = rule.validator ;
+                    if(fn && typeof fn === 'function'){
+                        self.cache.push(function(){
+                            return fn.call(dom,dom.value) ;
+                        } ) ;
+                    }
+                }
+            })(rule) ;
+        }
+    }
+    start () {
+        for (let validatorFn of this.cache){
+            var msg = validatorFn() ;
+            if( msg ){
+                return msg ;
+            }
+        }
+    }
+}
+
+var strategies = {
+    isNonEmpty (value,errorMsg){
+        if( value === '' ){
+            return errorMsg ;
+        }
+    },
+    minLength (value,length,errorMsg) {
+        if( value.length < length ){
+            return errorMsg ;
+        }
+    },
+    isMobile ( value,errorMsg ) {
+        if( !/(^1[3|5|8][0-9]{9}$)/.test(value) ){
+            return errorMsg ;
+        }
+    }
+} ;
