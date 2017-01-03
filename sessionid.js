d3416433f8a95bac0d57402a7605294d67a0e2a4
yichengjie <626659321@qq.com>
2017-01-03 16:16:11 +0800

    获取sessionid

---
+++ sessionid.js
@@ -0,0 +1,16 @@
+function getSessionId(){
+    var c_name = 'JSESSIONID';  
+    if(document.cookie.length>0){  
+        c_start=document.cookie.indexOf(c_name + "="); 
+        if(c_start!=-1){ 
+            c_start=c_start + c_name.length+1;
+            c_end=document.cookie.indexOf(";",c_start);  
+            if(c_end==-1) c_end=document.cookie.length;  
+            return document.cookie.substring(c_start,c_end);  
+        }else{
+            //手动从后台传来sessionid
+            var JSESSIONID = $('#JSESSIONID').val();
+            return JSESSIONID;
+        }
+    }
+}
